fc4befbfb34c63e1559972468c66b69e422a5599
federico-scaramelli <f.scaramelli0@gmail.com>
2022-03-25 21:57:35 +0100

    Test Suite

---
+++ Google_tests/GameSetupTest.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+TEST(Prova, Test1){
+  int a = 0;
+  ASSERT_EQ(a, 0);
+}
+
+TEST(Prova, Test2){
+  int a = 0;
+  ASSERT_EQ(a, 1);
+}
+
+//Alt+Insert per generazione automatica
