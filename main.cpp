b62a3c588ff906eb687ad45c495f1e1369d2a752
federico-scaramelli <f.scaramelli0@gmail.com>
2022-03-26 10:41:29 +0100

    Provaa2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main() {
   std::cout << "Hello, World!" << std::endl;
   std::cout << "Hi world" << std::endl;
+  std::cout << "Ciao Ivan! Benvenuto nel mio mondo." << std::endl;
 
   return 0;
 }
