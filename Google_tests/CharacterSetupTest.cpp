8154dfe611e973a1b7d9d4cde2088485765b2bf8
Federico Scaramelli <f.scaramelli0@gmail.com>
2022-03-30 18:03:14 +0200

    CharacterSetup push

---
+++ Google_tests/CharacterSetupTest.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include "../Character/Character.h"
+
+Character character = Character();
+
+
+TEST(CharacterSetup, CharacterSetup_CreationTest){
+    ASSERT_TRUE(character.isInitialized());
+}
+
+
+TEST(CharacterSetup, CharacterSetup_InizializationTest) {
+    character.setCharacteristics(10, 10, 10);
+    ASSERT_EQ(character.getStrength(), 10);
+    ASSERT_EQ(character.getEndurance(), 10);
+    ASSERT_EQ(character.getMind(), 10);
+}
+
+TEST(CharacterSetup, CharacterSetup_ClassAssignment) {
+    character.setClass("Warrior");
+    std::string characterClass = character.getCharacterClass();
+    ASSERT_EQ(characterClass, "Warrior");
+    CharacterCharacteristics classCharacteristics = characterClasses.find(characterClass)->second;
+    ASSERT_EQ(character.getStrength(), classCharacteristics.strength);
+    ASSERT_EQ(character.getEndurance(), classCharacteristics.endurance);
+    ASSERT_EQ(character.getMind(), classCharacteristics.mind);
+
+}
+//Alt+Insert per generazione automatica
---
+++ Utilities/utility.h
@@ -0,0 +1,10 @@
+//
+// Created by ossen on 30/03/2022.
+//
+
+#ifndef TEXT_ADVENTURE_GAME_UTILITY_H
+#define TEXT_ADVENTURE_GAME_UTILITY_H
+
+typedef unsigned int uint;
+
+#endif //TEXT_ADVENTURE_GAME_UTILITY_H
